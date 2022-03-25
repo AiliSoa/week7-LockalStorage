4439e1d7aec5db84098d43c7049ddda20fdb6b01
AiliSoa <asoa@tktk.ee>
2022-03-25 13:05:04 +0200

    fix liner gradient

--- script/app.js
+++ script/app.js
@@ -2,29 +2,40 @@
 const container = document.querySelector ('.container');
 const movieTitle = document.querySelector ('.userInputTitle');
 const moviePosterUrl = document.querySelector ('.userInputPosterUrl');
+const movieYear = document.querySelector ('.userInputYear');
 const btn = document.querySelector ('.button');
 const movieTitleToDisplay = document.querySelector ('.favouritMovieTitle');
 
 let titleInStorage = localStorage.getItem('title');
 let imageUrlInStorage = localStorage.getItem('imageUrl');
+let yearInStorage = localStorage.getItem('year');
 
 console.log(titleInStorage);
 console.log(imageUrlInStorage);
+console.log(yearInStorage);
 
-if(titleInStorage && imageUrlInStorage){
+if(titleInStorage && imageUrlInStorage && yearInStorage){
     movieTitleToDisplay.textContent = titleInStorage;
     container.style.backgroundImage = `linear-gradient(rgba(39, 39, 85, 0.63), rgba(87, 50, 50, 0.568)),
     url('${imageUrlInStorage}')`;
+    movieYear.textContent = yearInStorage;
+
+
 }
 
 btn.addEventListener('click', () => {
     let movieTitleInput = movieTitle.value;
     let posterUrlInput = moviePosterUrl.value;
+    let movieYearInput = movieYear.value;
     localStorage.setItem('title',movieTitleInput);
     localStorage.setItem('imageUrl', posterUrlInput);
+    localStorage.setItem('year',movieYearInput);
     movieTitleToDisplay.textContent = movieTitleInput;
     container.style.backgroundImage = `linear-gradient(rgba(39, 39, 85, 0.63), rgba(87, 50, 50, 0.568)),
     url('${posterUrlInput}')`;
+    movieYear.textContent = movieYearInput;
     movieTitle.value = '';
     moviePosterUrl.value = '';
+    movieYear.value = '';
+
 });
